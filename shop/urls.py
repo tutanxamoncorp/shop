1853621676460c8441a5b38af221096892eae331
Влад <vlados@example.com>
2026-05-03 17:47:23 +0300

    final structure: clean root and frontend

--- shop/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/register/', views.register),
-    path('api/login/', views.login_view),
-]
