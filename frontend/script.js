e14a7e085f027b4b0076578069db1ebdb2cab7e3
Влад <vlados@example.com>
2026-05-05 17:34:00 +0300

    Update

--- frontend/script.js
+++ frontend/script.js
@@ -17,7 +17,7 @@ function showLoader() {
     loader.style.display = 'flex';
     window.loaderTimer = setTimeout(() => {
         if (waitMsg) waitMsg.style.display = 'block';
-    }, 4000);
+    }, 4000); 
 }
 
 function hideLoader() {
